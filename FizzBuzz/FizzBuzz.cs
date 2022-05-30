7742802d090b6874f4ec2bd0a619f4a587c6b857
jordan-renaud <jordan.dj.renaud@gmail.com>
2022-05-30 12:41:05 +0100

    add divisible by numbers that checks through iterated numbers

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -2,14 +2,35 @@ namespace FizzBuzz
 {
     public class FizzBuzz
     {
-        // private string maxNumber;
+
+        private List<int> divisibleByNumbers = new List<int> { 3, 5 };
 
         public void DoFizzBuzz(int maxNumber)
         {
             for (int i = 1; i <= maxNumber; i++)
             {
-                Console.WriteLine(i);
+                if (checkForFizzBuzz(i))
+                {
+                    Console.WriteLine("doing a fizzbuzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+
             }
+        }
+
+        private bool checkForFizzBuzz(int number)
+        {
+            foreach (var divisibleByNumber in divisibleByNumbers)
+            {
+                if (number % divisibleByNumber == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
